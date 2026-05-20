cace8c6db12104adac920571fd756bf36d1ff610
EC2 Default User <ec2-user@ip-10-0-2-181.ap-south-1.compute.internal>
2026-05-20 13:09:13 +0000

    fix mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
+    await mongoose.connect('mongodb+srv://negiminakshi635_db_user:mJSWjQXS1cBmwOWC@cluster0.5krlbut.mongodb.net/food?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
        console.log('DB connected') ;
     })
 }
